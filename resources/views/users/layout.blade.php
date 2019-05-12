f9e1409323033b6d6e605f001e7dd6215e42be03
Warapon Panprateep <fluk.cirox@gmail.com>
2019-05-12 20:32:17 +0700

    update 12-05-62 test

--- resources/views/users/layout.blade.php
+++ resources/views/users/layout.blade.php
@@ -197,7 +197,7 @@
 
                 <!-- Modal Header -->
                 <div class="modal-header">
-                    <h4 class="modal-title"><strong>หมวดหมู่</strong></h4>
+                    <h4 class="modal-title"><strong>หมวดหมู่111</strong></h4>
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
                 </div>
 
